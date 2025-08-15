94ba1c0c43972f8a95f51e87c923edfea32c7b51
Peradorn43 <peam74110@gmail.com>
2025-08-15 11:41:03 +0700

    v2:test02-test05

---
+++ test02.py
@@ -0,0 +1,8 @@
+print('Hello...')
+print('Hi...');
+
+print('AAA');print('BBB');print('CCC')
+print('DDD');print('EEE');
+
+#--------------------------
+print(1234567)
---
+++ test03.py
@@ -0,0 +1,19 @@
+# คำสั่งแสดงผลข้อมูลใดๆ ทางหน้าจอ ใช้ฟังก์ชี่น Print ()   
+# Staring 
+print('Hello')
+print("HI")
+print('''Hey''')
+print("""Hum""")
+
+# Number 
+print(12345)    # integer/int
+print(454.458798)   #float
+
+# booLean ตรรกะ มีแค่ True กับ False 
+print(True) 
+print(False)
+
+#  Expression นิพจน์ 
+
+print(10 + 20 + 5)
+print(10 * 2 / 2)
